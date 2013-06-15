c902eac7ce95e39b349f7f6097d58cead06d9fb3
tomas <thenetcat@gmail.com>
2013-06-15 17:16:24 +0200

    Forgot to add Message file :-)

---
+++ DPOC/src/Message.cpp
@@ -0,0 +1,112 @@
+#include <sstream>
+#include <vector>
+
+#include "Config.h"
+#include "draw_text.h"
+#include "Message.h"
+
+static const size_t CHARS_PER_LINE = 30;
+
+std::vector<std::string> split_string(const std::string& str, char delim)
+{
+  std::istringstream istream(str);
+
+  std::string part;
+  std::vector<std::string> elements;
+  while (std::getline(istream, part, delim))
+  {
+    elements.push_back(part);
+  }
+
+  return elements;
+}
+
+Message& Message::instance()
+{
+  static Message message;
+  return message;
+}
+
+void Message::show(const std::string& msg)
+{
+  std::vector<std::string> strings = split_string(msg, ' ');
+
+  std::string buffer;
+  std::string complete;
+  int lines = 0;
+  for (auto it = strings.begin(); it != strings.end();)
+  {
+    std::string tmp = buffer + (*it);
+
+    if (tmp.size() > CHARS_PER_LINE)
+    {
+      if (buffer[buffer.size() - 1] == ' ')
+      {
+        buffer.resize(buffer.size() - 1);
+      }
+
+      complete += buffer + "\n";
+      buffer.clear();
+
+      lines++;
+      if (lines >= 4)
+      {
+        lines = 0;
+        m_pages.push(complete);
+        complete.clear();
+      }
+    }
+    else
+    {
+      buffer += (*it) + " ";
+      ++it;
+    }
+  }
+
+  if (!buffer.empty())
+    complete += buffer;
+
+  if (!complete.empty())
+    m_pages.push(complete);
+}
+
+void Message::nextPage()
+{
+  m_pages.pop();
+  m_currentBuffer.clear();
+  m_currentIndex = 0;
+}
+
+void Message::flush()
+{
+  if (m_pages.empty())
+    return;
+
+  while (m_currentIndex < m_pages.front().size())
+  {
+    update();
+  }
+}
+
+void Message::update()
+{
+  if (!m_pages.empty() && m_currentIndex < m_pages.front().size())
+  {
+    m_currentBuffer += m_pages.front()[m_currentIndex];
+
+    m_currentIndex++;
+  }
+}
+
+void Message::draw(sf::RenderTarget& target)
+{
+  sf::RectangleShape rect;
+  rect.setSize(sf::Vector2f(252, 46));
+  rect.setPosition(2, config::GAME_RES_Y - 48);
+  rect.setFillColor(sf::Color::Black);
+  rect.setOutlineColor(sf::Color::White);
+  rect.setOutlineThickness(2.0f);
+  target.draw(rect);
+
+  draw_text_bmp(target, 8, config::GAME_RES_Y - 48 + 8, "%s", m_currentBuffer.c_str());
+}
---
+++ DPOC/src/Message.h
@@ -0,0 +1,39 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include <string>
+#include <queue>
+
+#include <SFML/Graphics.hpp>
+
+class Message
+{
+public:
+  static Message& instance();
+  void show(const std::string& msg);
+  void nextPage();
+  void flush();
+
+  bool isVisible() const
+  {
+    return !m_pages.empty();
+  }
+  bool isWaitingForKey() const
+  {
+    if (m_pages.empty())
+      return false;
+
+    return m_currentIndex == m_pages.front().size();
+  }
+
+  void update();
+  void draw(sf::RenderTarget& target);
+private:
+  Message() : m_currentIndex(0) {}
+private:
+  std::queue<std::string> m_pages;
+  std::string m_currentBuffer;
+  size_t m_currentIndex;
+};
+
+#endif
