0e78edb46550bad585caa6354e489dc52ac1e680
tomas <thenetcat@gmail.com>
2013-07-11 12:00:33 +0200

    Function for actual text drawing.

--- DPOC/src/draw_text.cpp
+++ DPOC/src/draw_text.cpp
@@ -8,6 +8,37 @@ static sf::Font font;
 static sf::Text text;
 static sf::Texture* bmpFont;
 
+static void draw_bmp_text(sf::RenderTarget& target, int x, int y, const sf::Color& color, const std::string& str)
+{
+  sf::Sprite sprite;
+  sprite.setTexture(*bmpFont);
+
+  int px = x;
+  int py = y;
+  for (size_t i = 0; i < str.size(); i++)
+  {
+    char c = str[i];
+
+    if (c != '\n')
+    {
+      int charX = 8 * (c % (bmpFont->getSize().x / 8));
+      int charY = 8 * (c / (bmpFont->getSize().x / 8));
+
+      sprite.setTextureRect(sf::IntRect(charX, charY, 8, 8));
+      sprite.setPosition(px, py);
+      sprite.setColor(color);
+      target.draw(sprite);
+
+      px += 8;
+    }
+    else
+    {
+      px = x;
+      py += 8;
+    }
+  }
+}
+
 void init_text_drawing()
 {
   TRACE("Initialize text drawing.");
@@ -52,33 +83,7 @@ void draw_text_bmp(sf::RenderTarget& target, int x, int y, const char* fmt, ...)
 
   va_end(args);
 
-  sf::Sprite sprite;
-  sprite.setTexture(*bmpFont);
-
-  std::string str = buffer;
-  int px = x;
-  int py = y;
-  for (size_t i = 0; i < str.size(); i++)
-  {
-    char c = str[i];
-
-    if (c != '\n')
-    {
-      int charX = 8 * (c % (bmpFont->getSize().x / 8));
-      int charY = 8 * (c / (bmpFont->getSize().x / 8));
-
-      sprite.setTextureRect(sf::IntRect(charX, charY, 8, 8));
-      sprite.setPosition(px, py);
-      target.draw(sprite);
-
-      px += 8;
-    }
-    else
-    {
-      px = x;
-      py += 8;
-    }
-  }
+  draw_bmp_text(target, x, y, sf::Color::White, buffer);
 }
 
 void draw_text_bmp_ex(sf::RenderTarget& target, int x, int y, const sf::Color& color, const char* fmt, ...)
@@ -92,32 +97,5 @@ void draw_text_bmp_ex(sf::RenderTarget& target, int x, int y, const sf::Color& c
 
   va_end(args);
 
-  sf::Sprite sprite;
-  sprite.setTexture(*bmpFont);
-
-  std::string str = buffer;
-  int px = x;
-  int py = y;
-  for (size_t i = 0; i < str.size(); i++)
-  {
-    char c = str[i];
-
-    if (c != '\n')
-    {
-      int charX = 8 * (c % (bmpFont->getSize().x / 8));
-      int charY = 8 * (c / (bmpFont->getSize().x / 8));
-
-      sprite.setTextureRect(sf::IntRect(charX, charY, 8, 8));
-      sprite.setPosition(px, py);
-      sprite.setColor(color);
-      target.draw(sprite);
-
-      px += 8;
-    }
-    else
-    {
-      px = x;
-      py += 8;
-    }
-  }
+  draw_bmp_text(target, x, y, color, buffer);
 }
