5d856c2307f9dbacc608da44f66c9e1873cd08e7
tomas <thenetcat@gmail.com>
2013-07-11 11:57:26 +0200

    draw_text_ex with colors.

--- DPOC/src/draw_text.cpp
+++ DPOC/src/draw_text.cpp
@@ -80,3 +80,44 @@ void draw_text_bmp(sf::RenderTarget& target, int x, int y, const char* fmt, ...)
     }
   }
 }
+
+void draw_text_bmp_ex(sf::RenderTarget& target, int x, int y, const sf::Color& color, const char* fmt, ...)
+{
+  char buffer[512];
+
+  va_list args;
+  va_start(args, fmt);
+
+  vsprintf(buffer, fmt, args);
+
+  va_end(args);
+
+  sf::Sprite sprite;
+  sprite.setTexture(*bmpFont);
+
+  std::string str = buffer;
+  int px = x;
+  int py = y;
+  for (size_t i = 0; i < str.size(); i++)
+  {
+    char c = str[i];
+
+    if (c != '\n')
+    {
+      int charX = 8 * (c % (bmpFont->getSize().x / 8));
+      int charY = 8 * (c / (bmpFont->getSize().x / 8));
+
+      sprite.setTextureRect(sf::IntRect(charX, charY, 8, 8));
+      sprite.setPosition(px, py);
+      sprite.setColor(color);
+      target.draw(sprite);
+
+      px += 8;
+    }
+    else
+    {
+      px = x;
+      py += 8;
+    }
+  }
+}
--- DPOC/src/draw_text.h
+++ DPOC/src/draw_text.h
@@ -7,5 +7,6 @@ void init_text_drawing();
 
 void draw_text(sf::RenderTarget& target, int x, int y, const char* fmt, ...);
 void draw_text_bmp(sf::RenderTarget& target, int x, int y, const char* fmt, ...);
+void draw_text_bmp_ex(sf::RenderTarget& target, int x, int y, const sf::Color& color, const char* fmt, ...);
 
 #endif
