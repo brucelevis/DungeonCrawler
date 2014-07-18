7ab1018b9df1e07238ce19228a7e0ad8fdfa536f
tomas <thenetcat@gmail.com>
2014-07-19 00:24:43 +0200

    Wip6 bunch of stuff

--- DPOC/src/CharGen.cpp
+++ DPOC/src/CharGen.cpp
@@ -3,6 +3,7 @@
 #include "draw_text.h"
 #include "Sound.h"
 
+#include "StatusEffect.h"
 #include "Config.h"
 #include "Menu.h"
 #include "Frame.h"
@@ -10,6 +11,49 @@
 #include "PlayerClass.h"
 
 #include "CharGen.h"
+
+static sf::RectangleShape make_select_rect(int x, int y, int w, int h, sf::Color color = sf::Color::Red)
+{
+  sf::RectangleShape rect;
+  rect.setFillColor(sf::Color::Transparent);
+  rect.setOutlineThickness(1.0f);
+  rect.setOutlineColor(color);
+  rect.setSize(sf::Vector2f(w, h));
+  rect.setPosition(x, y);
+
+  return rect;
+}
+
+static void draw_status(sf::RenderTarget& target, PlayerCharacter* character, int x, int y)
+{
+  draw_frame(target, 16, 16, 14*16, 13*16);
+
+  character->draw(target, x, y);
+
+  draw_text_bmp_ex(target, x + 40, y,
+      get_status_effect(character->getStatus())->color,
+      "%s (%s)", character->getName().c_str(), character->getStatus().c_str());
+  draw_text_bmp(target, x + 40, y + 12, "Hp: %d/%d", character->getAttribute("hp").current, character->getAttribute("hp").max);
+  draw_text_bmp(target, x + 40, y + 24, "Mp: %d/%d", character->getAttribute("mp").current, character->getAttribute("mp").max);
+
+//  draw_text_bmp(target, x + 40 + 96, y + 12, "Lv: %d", character->computeCurrentAttribute("level"));
+//  draw_text_bmp(target, x + 40 + 96, y + 24, "Tn: %d", character->toNextLevel());
+
+  y += 40;
+
+  draw_text_bmp(target, x, y,      "Strength: %d", character->computeCurrentAttribute("strength"));
+  draw_text_bmp(target, x, y + 12, "Defense:  %d", character->computeCurrentAttribute("defense"));
+  draw_text_bmp(target, x, y + 24, "Magic:    %d", character->computeCurrentAttribute("magic"));
+  draw_text_bmp(target, x, y + 36, "Mag.Def:  %d", character->computeCurrentAttribute("mag.def"));
+  draw_text_bmp(target, x, y + 48, "Speed:    %d", character->computeCurrentAttribute("speed"));
+  draw_text_bmp(target, x, y + 60, "Luck:     %d", character->computeCurrentAttribute("luck"));
+
+//  for (size_t i = 0; i < PlayerCharacter::equipNames.size(); i++)
+//  {
+//    Item* item = character->getEquipment(PlayerCharacter::equipNames[i]);
+//    draw_text_bmp(target, x, y + 84 + 12 * i, "%s: %s", PlayerCharacter::equipNames[i].c_str(), item ? item->name.c_str(): "");
+//  }
+}
 
 struct SelectClassMenu : public Menu
 {
@@ -26,7 +70,7 @@ struct SelectClassMenu : public Menu
 
   void handleConfirm()
   {
-
+    setVisible(false);
   }
 
   void draw(sf::RenderTarget& target, int x, int y)
@@ -55,9 +99,17 @@ private:
 
 struct GenerateMenu : public Menu
 {
+  enum State
+  {
+    STATE_DEFAULT,
+    STATE_ENTER_NAME,
+    STATE_SELECT_CLASS
+  };
+
   GenerateMenu() :
-   m_classes(get_all_classes()),
-   m_selectClassMenu(m_classes)
+    m_classes(get_all_classes()),
+    m_selectClassMenu(m_classes),
+    m_state(STATE_DEFAULT)
   {
     m_selectClassMenu.setVisible(false);
 
@@ -68,9 +120,59 @@ struct GenerateMenu : public Menu
 
   void handleConfirm()
   {
-    if (getCurrentMenuChoice() == "Select class")
+    if (m_state == STATE_DEFAULT)
     {
-      m_selectClassMenu.setVisible(true);
+      std::string currentChoice = getCurrentMenuChoice();
+
+      if (currentChoice == "Select class")
+      {
+        m_selectClassMenu.setVisible(true);
+        m_state = STATE_SELECT_CLASS;
+      }
+      else if (currentChoice == "Enter name")
+      {
+
+      }
+      else if (currentChoice == "Done")
+      {
+        if (theName.size() && theClass.size())
+        {
+          setVisible(false);
+        }
+        else
+        {
+          play_sound(config::get("SOUND_CANCEL"));
+        }
+      }
+    }
+    else if (m_state == STATE_SELECT_CLASS)
+    {
+      m_selectClassMenu.handleConfirm();
+      if (!m_selectClassMenu.isVisible())
+      {
+        theClass = m_selectClassMenu.getCurrentMenuChoice();
+        m_state = STATE_DEFAULT;
+      }
+    }
+  }
+
+  void handleEscape()
+  {
+    if (m_state == STATE_DEFAULT)
+    {
+      setVisible(false);
+      theName = "";
+      theClass = "";
+      resetChoice();
+    }
+    else if (m_state == STATE_SELECT_CLASS)
+    {
+      m_selectClassMenu.handleEscape();
+      m_state = STATE_DEFAULT;
+    }
+    else if (m_state == STATE_ENTER_NAME)
+    {
+
     }
   }
 
@@ -88,7 +190,12 @@ struct GenerateMenu : public Menu
 
   void draw(sf::RenderTarget& target, int x, int y)
   {
+    draw_frame(target, 0, 0, config::GAME_RES_X, config::GAME_RES_Y);
+
     Menu::draw(target, x, y);
+
+    draw_text_bmp(target, 8, 8,  "Name:  %s", theName.c_str());
+    draw_text_bmp(target, 8, 20, "Class: %s", theClass.c_str());
 
     if (m_selectClassMenu.isVisible())
     {
@@ -96,9 +203,84 @@ struct GenerateMenu : public Menu
     }
   }
 
+  std::string theClass;
+  std::string theName;
+
 private:
   std::vector<PlayerClass> m_classes;
   SelectClassMenu m_selectClassMenu;
+  State m_state;
+};
+
+struct CharGenCharacterMenu : public Menu
+{
+  CharGenCharacterMenu(Player* player)
+    : m_player(player)
+  {
+    setCursorVisible(false);
+  }
+
+  void handleConfirm()
+  {
+  }
+
+  void handleEscape()
+  {
+    setCursorVisible(false);
+    resetChoice();
+  }
+
+  int getWidth() const
+  {
+    return Menu::getWidth() + 32;
+  }
+
+  int getHeight() const
+  {
+    return (4 + 32) * getNumberOfChoice();
+  }
+
+  void refresh()
+  {
+    clear();
+
+    const std::vector<PlayerCharacter*>& party = m_player->getParty();
+
+    for (auto it = party.begin(); it != party.end(); ++it)
+    {
+      addEntry((*it)->getName());
+    }
+  }
+
+  void draw(sf::RenderTarget& target, int x, int y)
+  {
+    draw_frame(target, x + 72, y, 184, 240);
+
+    for (int i = 0; i < getNumberOfChoice(); i++)
+    {
+      PlayerCharacter* character = m_player->getCharacter(getChoice(i));
+
+      int offX = x + 8 + 5 * 16;
+      int offY = y + 8;
+
+      character->draw(target, offX, offY + i * 48);
+
+      draw_text_bmp_ex(target, offX + 40, offY + i * 48,
+          get_status_effect(character->getStatus())->color,
+          "%s (%s)", character->getName().c_str(), character->getStatus().c_str());
+      draw_text_bmp(target, offX + 40, offY + i * 48 + 12, "Hp: %d/%d", character->getAttribute("hp").current, character->getAttribute("hp").max);
+      draw_text_bmp(target, offX + 40, offY + i * 48 + 24, "Mp: %d/%d", character->getAttribute("mp").current, character->getAttribute("mp").max);
+
+      if (cursorVisible() && getCurrentChoiceIndex() == i)
+      {
+        sf::RectangleShape rect = make_select_rect(offX - 2, offY + i * 48 - 2, 164, 36);
+        target.draw(rect);
+      }
+    }
+  }
+
+private:
+  Player* m_player;
 };
 
 struct SelectMenu : public Menu
@@ -113,7 +295,8 @@ struct SelectMenu : public Menu
 
   SelectMenu(Player* player)
    : m_state(STATE_DEFAULT),
-     m_player(player)
+     m_player(player),
+     m_characterMenu(m_player)
   {
     addEntry("Add");
     addEntry("Inspect");
@@ -156,6 +339,7 @@ struct SelectMenu : public Menu
         {
           m_state = STATE_REMOVE;
           m_characterMenu.setCursorVisible(true);
+          m_characterMenu.resetChoice();
         }
         else
         {
@@ -169,6 +353,17 @@ struct SelectMenu : public Menu
       break;
     case STATE_ADD:
       m_genMenu.handleConfirm();
+
+      if (!m_genMenu.isVisible())
+      {
+        m_state = STATE_DEFAULT;
+
+        if (m_genMenu.theClass.size() && m_genMenu.theName.size())
+        {
+          m_player->addNewCharacter(m_genMenu.theName, m_genMenu.theClass, 0, 0, 1);
+          m_characterMenu.refresh();
+        }
+      }
       break;
     case STATE_INSPECT:
     case STATE_REMOVE:
@@ -176,16 +371,50 @@ struct SelectMenu : public Menu
 
       if (m_state == STATE_INSPECT)
       {
-        // TODO: Draw summary.
+        m_inspectChar = currentChoice;
       }
       else if (m_state == STATE_REMOVE)
       {
-        get_player()->removeCharacter(currentChoice);
+        m_player->removeCharacter(currentChoice);
         m_characterMenu.refresh();
-        m_state = STATE_DEFAULT;
-        m_characterMenu.setCursorVisible(false);
+
+        if (m_player->getParty().empty())
+        {
+          m_state = STATE_DEFAULT;
+          m_characterMenu.setCursorVisible(false);
+        }
       }
 
+      break;
+    }
+  }
+
+  void handleEscape()
+  {
+    switch (m_state)
+    {
+    case STATE_DEFAULT:
+      break;
+    case STATE_ADD:
+      m_genMenu.handleEscape();
+      if (!m_genMenu.isVisible())
+      {
+        m_state = STATE_DEFAULT;
+      }
+      break;
+    case STATE_INSPECT:
+      if (m_inspectChar.empty())
+      {
+        m_state = STATE_DEFAULT;
+      }
+      else
+      {
+        m_inspectChar = "";
+      }
+      break;
+    case STATE_REMOVE:
+      m_characterMenu.handleEscape();
+      m_state = STATE_DEFAULT;
       break;
     }
   }
@@ -221,13 +450,20 @@ struct SelectMenu : public Menu
 
       Menu::draw(target, x, y);
     }
+
+    if (m_state == STATE_INSPECT && m_inspectChar.size())
+    {
+      draw_status(target, m_player->getCharacter(m_inspectChar), 8, 8);
+    }
   }
 
 private:
   State m_state;
-  CharacterMenu m_characterMenu;
   GenerateMenu m_genMenu;
   Player* m_player;
+  CharGenCharacterMenu m_characterMenu;
+
+  std::string m_inspectChar;
 };
 
 CharGen::CharGen()
